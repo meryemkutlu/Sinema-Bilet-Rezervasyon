169954075ca4ba26136ff598f90b68bf370f49fc
meryemkutlu <meryem.kutlu13@gmail.com>
2022-12-13 22:08:06 +0300

    git remote add origin git@github.com:meryemkutlu/Sinema-Bilet-Rezervasyon.git

---
+++ index.js
@@ -0,0 +1,82 @@
+const container = document.querySelector('.container');
+const count = document.getElementById('count');
+const amount = document.getElementById('amount');
+const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
+
+getFromLocalStorage();
+calculateTotal();
+
+
+container.addEventListener('click', function (e) {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
+        e.target.classList.toggle('selected');
+
+
+        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+        let price = select.value;
+        count.innerText = selectedSeatCount;
+        amount.innerText = selectedSeatCount * price;
+
+    }
+});
+
+
+select.addEventListener('change', function (e) {
+    calculateTotal();
+
+});
+
+function calculateTotal() {
+    const selectedseats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+    selectedseats.forEach(function (seat) {
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function (seat) {
+        seatsArr.push(seat);
+    });
+
+    let selectedSeatIndexs = selectedSeatsArr.map(function (seat) {
+        return seatsArr.indexOf(seat);
+    });
+
+    let selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocolStorage(selectedSeatindexs);
+}
+
+function getFromLocalStorage() {
+const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+if (selectedSeats !=null && selectedSeats.length > 0 ) {
+    seats.forEach(function(seat,index) {
+    if (selectedSeats.indexOf(index)> -1 ) {
+        seat.classList.add('selected');
+    }
+   });
+}
+
+const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+if (selectedMovieIndex != null ){
+    select.selectedIndex = selectedMovieIndex ;
+
+    }
+}
+
+
+
+
+function saveToLocolStorage(indexs) {
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+     localStorage.setItem('selectedMovieIndex',select.selectedIndex);
+
+}
+
+
